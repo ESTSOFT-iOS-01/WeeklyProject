4a1bff3c298a4d515e358e872821c729973570fb
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-02-10 17:56:54 +0900

    #11 merge conflict test

--- doyeon/Weekly01/Weekly01/main.swift
+++ doyeon/Weekly01/Weekly01/main.swift
@@ -48,3 +48,22 @@ func getUserInput() {
 }
 
 getUserInput()
+
+//
+//  main.swift
+//  Weekly01
+//
+//  Created by 김도연 on 2/7/25.
+//
+//
+//  main.swift
+//  Weekly01
+//
+//  Created by 김도연 on 2/7/25.
+//
+//
+//  main.swift
+//  Weekly01
+//
+//  Created by 김도연 on 2/7/25.
+//
